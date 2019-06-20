dd6a3d26922fff7c28c54a3284fb1f7b5d83bebd
bossdm <bossdm@github.com>
2019-06-20 03:53:28 +0100

    complete iridis script

--- make_all_simulation.sh
+++ make_all_simulation.sh
@@ -7,9 +7,9 @@ for dim in 2 10 14 21 400; do
     cd build_${dim}D
     if [ $dim -gt 3 ]
     then
-    cmake -DRECORD_FITNESS=ON -DCVT_USAGE=ON -DBD=${dim}  ..
+    cmake -DCVT_USAGE=ON -DBD=${dim}  ..
     else
-    cmake -DRECORD_FITNESS=ON -DBD=${dim} ..
+    cmake -DBD=${dim} ..
     fi
    make 
 
