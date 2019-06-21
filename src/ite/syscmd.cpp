8fa3dfd1e4b855807eecdf04eeb4a6220aecded1
Danesh Tarapore <daneshtarapore@gmail.com>
2019-06-21 15:25:36 +0100

    Cleaned up src/ite directory

--- src/ite/syscmd.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdlib>
-#include <fstream>
-#include <iostream>
- 
-int main()
-{
-    std::system("./bin/baseline_behaviour experiments/baseline-behavs.argos"); // execute the UNIX command "ls -l >test.txt"
-    std::cout << "Program ended";    
-}
