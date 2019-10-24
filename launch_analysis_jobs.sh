734daee0a8a492fe01d12525a6275d21a9db560a
bossdm <davidmbossens@gmail.com>
2019-10-24 11:20:37 +0100

    change output dir of fault results

--- launch_analysis_jobs.sh
+++ launch_analysis_jobs.sh
@@ -59,7 +59,7 @@ time["Flocking"]=400
 perturbations_folder="experiments/perturbations"
 # for FaultType in "FAULT_PROXIMITYSENSORS_SETMIN" "FAULT_PROXIMITYSENSORS_SETMAX" "FAULT_PROXIMITYSENSORS_SETRANDOM" \
 # "FAULT_ACTUATOR_LWHEEL_SETHALF" "FAULT_ACTUATOR_RWHEEL_SETHALF" "FAULT_ACTUATOR_BWHEELS_SETHALF"; do
-for FaultIndex in $(seq 10 11); do
+for FaultIndex in $(seq 0 1); do
 	SimTime=${time[${FitfunType}]}
 	echo "simtime"${SimTime}
 	for FaultID in "-1"; do
@@ -100,13 +100,13 @@ for FaultIndex in $(seq 10 11); do
 							echo "will look for perturbations at run${Replicates}_p${FaultIndex}"
 							ConfigFolder=${Base}
 							ConfigFile=${ConfigFolder}/history_exp_${Replicates}_p${FaultIndex}.argos # just to write the history
-							ArchiveDir=${ConfigFolder}/run${Replicates}_p${FaultIndex}/results${SUFFIX}
+							ArchiveDir=${ConfigFolder}/faultyrun${Replicates}_p${FaultIndex}/results${SUFFIX}
 							export Outfolder=${ArchiveDir} # where to look for the best result and to output the results
 							FaultType=FAULT_NONE
 						else
 							# look at archive dir at previous perturbation results; config is at FAULT_NONE
 							FaultType="FILE:${perturbations_folder}/run${Replicates}_p${FaultIndex}.txt"
-							ConfigFolder=${Base}/run${Replicates}_p${FaultIndex}
+							ConfigFolder=${Base}/faultyrun${Replicates}_p${FaultIndex}
 							mkdir -p ${ConfigFolder}
 							ConfigFile=${ConfigFolder}/exp_${SUFFIX}.argos
 							export ArchiveDir=${Base}/results${SUFFIX} # point to the generation file and archive
