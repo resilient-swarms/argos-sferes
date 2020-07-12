30c665af90df00c48d256ba6fe7370ef9e0ed63e
bossdm <davidmbossens@gmail.com>
2020-07-12 02:30:26 +0100

    add missing file

---
+++ cmake_scripts/make_all_largesim.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+for dim in 3 ; do   # 10 14 21 400 
+    cd ~/argos-sferes
+    mkdir build_${dim}D
+    cd build_${dim}D
+    rm -rf *
+    if [ $dim -gt 3 ]; then
+    
+      cmake -DLARGE=ON -DNN_INPUT_TYPE=1 -DCMAKE_BUILD_TYPE=Debug -DCVT_USAGE=ON -DBD=${dim}  ..
+    else
+      cmake -DLARGE=ON -DNN_INPUT_TYPE=1 -DCMAKE_BUILD_TYPE=Debug -DBD=${dim} ..
+    fi
+   make -j 2
+
+
+done 
