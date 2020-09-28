894074dc2209735fdbc670a94a3a8f788c2cd117
bossdm <davidmbossens@gmail.com>
2020-09-28 23:50:48 +0100

    correct UCBID -> UCB_ID

--- launch_foraging_BO_array.sh
+++ launch_foraging_BO_array.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 datafolder=$1
-
+if [ ! -z "${datafolder}" ]; then
+	echo "Error: no datafolder given"
+	exit 125
+fi
 declare -A acq_funs
 acq_funs[0]="UCB"
 acq_funs[1]="UCB_ID"
 declare -A BO_exps
-BO_exps["UCB"]=BO_single
-BO_exps["UCB_ID"]=BO_single_IDprior
+BO_exps["UCB"]="BO_single"
+BO_exps["UCB_ID"]="BO_single_IDprior"
 for alpha in 0.05 0.25 0.50 1; do
     for l in 0.05 0.1 0.2 0.4 1; do
         for acq in ${!acq_funs[@]}; do
