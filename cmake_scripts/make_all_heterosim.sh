e7399319059e7f13aab47a92da06c9885a1f4d71
bossdm <davidmbossens@gmail.com>
2020-09-28 18:23:21 +0100

    experiments cmake-script

--- cmake_scripts/make_all_heterosim.sh
+++ cmake_scripts/make_all_heterosim.sh
@@ -3,24 +3,8 @@
 
 echo "ACQ=$1"
 echo "KERN=$2"
-# first make a binary for the recording of fitness
-for dim in 3 ; do   # 10 14 21 400 
-    cd ~/argos-sferes
-    mkdir build_${dim}D
-    cd build_${dim}D
-    rm -rf *
-    if [ $dim -gt 3 ]; then
-    
-      cmake -DBO_ACQ=$1 -DBO_KERN=$2 -DRECORD_FITNESS=ON -DHETEROGENEOUS=ON -DNN_INPUT_TYPE=1 -DCMAKE_BUILD_TYPE=Debug -DCVT_USAGE=ON -DBD=${dim}  ..
-    else
-      cmake -DBO_ACQ=$1 -DBO_KERN=$2 -DRECORD_FITNESS=ON -DHETEROGENEOUS=ON -DNN_INPUT_TYPE=1 -DCMAKE_BUILD_TYPE=Debug -DBD=${dim} ..
-    fi
-   make -j 2
-
-
-done 
-
-
+echo "LIMBO_ALPHA=$3"
+echo "LIMBO_L=$4"
 # then make a binary for the search
 for dim in 3 ; do   # 10 14 21 400 
     cd ~/argos-sferes
@@ -29,12 +13,11 @@ for dim in 3 ; do   # 10 14 21 400
     rm -rf *
     if [ $dim -gt 3 ]; then
     
-      cmake -DBO_ACQ=$1 -DBO_KERN=$2 -DHETEROGENEOUS=ON -DNN_INPUT_TYPE=1 -DCMAKE_BUILD_TYPE=Debug -DCVT_USAGE=ON -DBD=${dim}  ..
+      cmake -DBO_ACQ=$1 -DBO_KERN=$2 -DLIMBO_ALPHA=$3 -DLIMBO_L=$4 -DHETEROGENEOUS=ON -DNN_INPUT_TYPE=1 -DCMAKE_BUILD_TYPE=Debug -DCVT_USAGE=ON -DBD=${dim}  ..
     else
-      cmake -DBO_ACQ=$1 -DBO_KERN=$2 -DHETEROGENEOUS=ON -DNN_INPUT_TYPE=1 -DCMAKE_BUILD_TYPE=Debug -DBD=${dim} ..
+      cmake -DBO_ACQ=$1 -DBO_KERN=$2 -DLIMBO_ALPHA=$3 -DLIMBO_L=$4 -DHETEROGENEOUS=ON -DNN_INPUT_TYPE=1 -DCMAKE_BUILD_TYPE=Debug -DBD=${dim} ..
     fi
    make -j 2
-
 
 done 
 
