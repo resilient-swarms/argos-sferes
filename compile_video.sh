05a2d5eb83e3cc315c41290210e81c96bf3802af
bossdm <davidmbossens@gmail.com>
2020-10-07 17:36:35 +0100

    add noID and random ID config

--- compile_video.sh
+++ compile_video.sh
@@ -2,7 +2,7 @@
 
 echo "will compile video ${1}.mp4"
 
-ffmpeg -r 24 -i frame_%5d.png -vcodec libx264 -y -an ${1}.mp4 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2"
+ffmpeg -r 24 -i ~/argos-sferes/frame_%5d.png -vcodec libx264 -y -an ${1}.mp4 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2"
 
 # clean up the directory
-rm frame_*.png
+#rm frame_*.png
