02727133470590d2897766676d8cc8669b40296b
bossdm <davidmbossens@gmail.com>
2021-03-09 14:00:31 +0000

    modify launch

--- launch_foraging_BO_array.sh
+++ launch_foraging_BO_array.sh
@@ -6,12 +6,12 @@ if [ -z "${datafolder}" ]; then
 	exit 125
 fi
 declare -A BO_exps
-acq_funs[0]="UCB_LOCAL"
+acq_funs[0]="UCB_LOCAL2"
 #acq_funs[1]="UCB_LOCAL"
 #acq_funs[3]="UCB"
 #acq_funs[1]="UCB_ID"
 
-BO_exps["UCB_LOCAL"]="BO_single_multi"
+BO_exps["UCB_LOCAL2"]="BO_single_multi"
 #BO_exps["UCB_LOCAL"]="BO_single_multi_record"
 #BO_exps["UCB_ID"]="BO_single_IDprior"
 for alpha in 0.93; do
